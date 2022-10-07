86e352805625aa94d865704eabeeaaf4ab3e9c85
Aradhya-Shaswat <reddye1120@gmail.com>
2022-10-07 09:42:07 +0530

    EOL

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <b>Friendbase</b> has Reached its <u>End of Life</u> and it will be no longer continued.
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/Fib-Inc/friendbaseinc"
+          target=""
+          rel=""
+        >
+          Source Code
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
